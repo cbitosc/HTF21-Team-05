42e23cb1d2f9d1ccbd0c6aa4ea973029ffa71489
Dhruv Saxena <dhruv162002@gmail.com>
2021-10-30 01:23:15 +0530

    Sign up and Sign in complete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 // const https = require('https');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('images'));
 
 
@@ -15,8 +16,20 @@ app.get("/studentSignin.html", function(req, res){
     res.sendFile(__dirname + "/mainpage/studentSignin.html");
 });
 
-app.get("/studentSignin.html", function(req, res){
-    res.sendFile(__dirname + "/mainpage/studentSignin.html");
+app.post("/studentSignin.html", function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username, password);
+});
+
+app.get("/clubSignin.html", function(req, res){
+    res.sendFile(__dirname + "/mainpage/clubSignin.html");
+});
+
+app.post("/clubSignin.html", function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username, password);
 });
 
 app.listen(3000, function() {
